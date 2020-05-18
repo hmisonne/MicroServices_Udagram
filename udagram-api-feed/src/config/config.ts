42d6a85659e975b22b8ff963f1cc17d0f0544f1f
hmisonne <helene.misonne@gmail.com>
2020-05-18 14:59:48 -0700

    Enter config

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'helene',
+  'password': 'buierag78a53',
+  'database': 'postgres',
+  'host': 'cdr.ccffce8cbcx2.us-west-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL2,
+  'aws_region': 'us-west-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'test-376262390406-dev',
+  'url': 'http://a218bf21f08964f9dabd85fd2a11587a-423112248.us-west-2.elb.amazonaws.com/',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'hello',
   },
 };
