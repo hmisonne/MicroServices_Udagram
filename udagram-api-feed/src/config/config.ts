b2c435ca39006f89620ced4fac96dd6b3343ad8e
hmisonne <helene.misonne@gmail.com>
2020-05-18 13:46:53 -0700

    Updated config

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'url': process.env.URL2,
   'jwt': {
     'secret': process.env.JWT_SECRET,
   },
